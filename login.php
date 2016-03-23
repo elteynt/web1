29f3b49d44640ab98564608d7e42469ead783a49
elteynt <elteynt@yahoo.com.tw>
2016-03-23 12:19:21 +0800

    commit1

---
+++ login.php
@@ -0,0 +1,56 @@
+﻿<?php
+session_start();
+$host="localhost";
+$db_user="root";//用户名
+$db_pass="";//密码
+$db_name="test";//数据库
+//$timezone="Asia/Taipei";
+//$code = trim($_POST['code']);
+$_SESSION['cart'] = array();
+//if($code==$_SESSION["helloweba_num"]){
+
+$conn = mysqli_connect($host, $db_user, $db_pass, $db_name);
+// Check connection
+if (!$conn) {
+    die("Connection failed: "  . mysqli_connect_error());
+} 
+mysqli_set_charset($conn, 'utf8');
+$username = stripslashes(trim($_POST['name']));
+//检测用户名是否存在
+$sql = "select * from customer where username='$username'";
+$result = mysqli_query($conn, $sql);
+
+if(mysqli_num_rows($result) == 0){
+	$conn->close();
+	echo '<h1>帳號不存在，請換其他帳號</h1><br>'; header('Refresh: 3; url=hw1.html');die;
+}
+$row = mysqli_fetch_assoc($result);
+
+$password = md5(trim($_POST['password']));
+if( strcasecmp($row["password"], $password )!= 0){
+    $conn->close();
+	echo '<h1>密碼輸入錯誤。</h1><br>'; header('Refresh: 3; url=index.php');die;
+} 
+if (strcasecmp($sername,"root" )!= 0)
+{
+	
+mysqli_close($conn);
+$_SESSION["username"]=$username;
+echo '<h1>歡迎會員'. $_SESSION["username"] . '</h1><br>';
+header('Refresh: 5; url=hw1.html');die;
+
+
+}
+else
+{
+mysqli_close($conn);
+$_SESSION["username"]=$username;
+echo '<h1>歡迎管理員'. $_SESSION["username"] . '</h1><br>';
+header('Refresh: 5; url=hw1.html');die;
+
+}
+
+
+?>
+
+
---
+++ shop.php
@@ -0,0 +1,48 @@
+﻿<?php
+session_start();
+
+
+if( trim($_SESSION['username']) == '')
+{ echo '<h1>請先登入會員</h1><br>'; 
+header('Refresh: 5; url=hw1.html');die;
+die; }
+
+echo '<h1>會員：'. $_SESSION["username"] . '</h1><br>';
+if (isset($_GET['Items']))
+{
+array_push($_SESSION['cart'],$_GET['Items']);	
+}
+$cart = $_SESSION['cart'];
+$cartlength = count($cart);
+$arr = array(0,0,0,0,0,0);
+echo '購買順序：<br>';
+for ($i = 0 ; $i < $cartlength ; $i++)
+{
+ echo '商品'.$cart[$i].',';
+ if(strcmp ($cart[$i],"1") == 0) $arr[0]++;
+ if(strcmp ($cart[$i],"2") == 0) $arr[1]++;
+ if(strcmp ($cart[$i],"3") == 0) $arr[2]++;
+ if(strcmp ($cart[$i],"4") == 0) $arr[3]++;
+ if(strcmp ($cart[$i],"5") == 0) $arr[3]++;
+ if(strcmp ($cart[$i],"6") == 0) $arr[3]++;
+}
+echo '<br>';
+echo '<br>';
+echo '<table border="1"><tr>';
+echo '<td>一號商品</td>';
+echo '<td>'. $arr[0].'</td></tr>';
+echo '<tr><td>二號商品</td>';
+echo '<td>'. $arr[1].'</td></tr>';
+echo '<tr><td>三號商品</td>';
+echo '<td>'. $arr[2].'</td></tr>';
+echo '<tr><td>四號商品</td>';
+echo '<td>'. $arr[3].'</td></tr>';
+echo '<tr><td>五號商品</td>';
+echo '<td>'. $arr[4].'</td></tr>';
+echo '<tr><td>六號商品</td>';
+echo '<td>'. $arr[5].'</td></tr>';
+echo '</table>';
+
+echo '<br>';
+echo '<br>';
+echo '<form action="hw1.html"><input type="submit" value="返回" ></form>';
